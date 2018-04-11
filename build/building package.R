6642b31340db598636b004ed1bb1dbdccd0fa58e
Evgeny Metelkin <evgeny.metelkin@gmail.com>
2018-04-11 13:00:52 +0300

    add notes to parsetgen function

--- build/building package.R
+++ build/building package.R
@@ -5,15 +5,18 @@
 setwd("Y:\\dbs-package")
 
 library(devtools)
-library(dbs)
+# library(dbs)
 
+### Just build and install the package from current directory
+install() # build and install
+
+### Install the latest version from github
+install_github("insysbio/dbs-package")
+
+### ???
 out <- check(manual=TRUE)
 write(out[[2]], "warn.log")
 
 build(path = "dist", binary = TRUE, manual=TRUE) # build for windows binary = TRUE
 
-install() # build and install
-
-install_github("insysbio/dbs-package")
-
-#load_all() # 
+#load_all()
